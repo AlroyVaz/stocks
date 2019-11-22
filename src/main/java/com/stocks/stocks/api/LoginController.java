25195a09122ac557ed15bb6526dc19b764d64097
AshWije <ashiniwije126@gmail.com>
2019-11-22 15:08:03 -0600

    Create LoginController.java

---
+++ src/main/java/com/stocks/stocks/api/LoginController.java
@@ -0,0 +1,28 @@
+package com.stocks.stocks.api;
+
+import com.stocks.stocks.model.Credentials;
+import com.stocks.stocks.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+@RequestMapping("login")
+@RestController
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @Autowired
+    public LoginController(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @PostMapping
+    public boolean login(@RequestBody Credentials credentials, HttpSession session) {
+        return loginService.login(credentials, session);
+    }
+}
