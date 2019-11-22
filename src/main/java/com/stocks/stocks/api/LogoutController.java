141a50cb579662b895ecc32b823426934a82b682
AshWije <ashiniwije126@gmail.com>
2019-11-22 15:46:50 -0600

    Create LogoutController.java

---
+++ src/main/java/com/stocks/stocks/api/LogoutController.java
@@ -0,0 +1,25 @@
+package com.stocks.stocks.api;
+
+import com.stocks.stocks.service.LogoutService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequestMapping("logout")
+@RestController
+public class LogoutController {
+    private final LogoutService logoutService;
+
+    @Autowired
+    public LogoutController(LogoutService logoutService) {
+        this.logoutService = logoutService;
+    }
+
+    @GetMapping
+    public void logout(HttpServletRequest request) {
+            logoutService.logout(request);
+    }
+}
