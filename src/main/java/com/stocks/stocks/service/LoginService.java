9ff97072579e12522a4784724261beb41879c85c
AshWije <ashiniwije126@gmail.com>
2019-11-28 14:16:07 -0600

    Update LoginService.java

--- src/main/java/com/stocks/stocks/service/LoginService.java
+++ src/main/java/com/stocks/stocks/service/LoginService.java
@@ -2,10 +2,12 @@ package com.stocks.stocks.service;
 
 import com.stocks.stocks.dao.LoginDao;
 import com.stocks.stocks.model.Credentials;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 @Service
@@ -18,9 +20,15 @@ public class LoginService {
         this.loginDao = loginDao;
     }
 
-    public boolean login(Credentials credentials, HttpSession session) {
-        if(loginDao.validateUser(credentials)) {
+    public boolean login(Credentials credentials, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        // not already logged in (must log out first)
+        if (session != null && session.getAttribute("USERNAME") != null)
+            return false;
+        ObjectId userId = loginDao.validateUser(credentials);
+        if(userId != null) {
             session.setAttribute("USERNAME", credentials.getUsername());
+            session.setAttribute("USER_ID", userId);
             return true;
         }
         return false;
