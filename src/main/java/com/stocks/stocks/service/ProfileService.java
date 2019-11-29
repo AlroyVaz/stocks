87d4a97281307f12cf46daa1c0c9af788a00bb9e
AshWije <ashiniwije126@gmail.com>
2019-11-29 13:45:26 -0600

    Update ProfileService.java

--- src/main/java/com/stocks/stocks/service/ProfileService.java
+++ src/main/java/com/stocks/stocks/service/ProfileService.java
@@ -2,7 +2,6 @@ package com.stocks.stocks.service;
 
 import com.stocks.stocks.dao.UserDao;
 import com.stocks.stocks.model.User;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
