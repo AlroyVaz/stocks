68898ae775667245507f5c1894f220805f5b91d6
AshWije <ashiniwije126@gmail.com>
2019-11-29 13:38:47 -0600

    Update UserDao.java

--- src/main/java/com/stocks/stocks/dao/UserDao.java
+++ src/main/java/com/stocks/stocks/dao/UserDao.java
@@ -19,20 +19,25 @@ public class UserDao {
         this.mongoTemplate = mongoTemplate;
     }
 
-    public ObjectId validateUser(Credentials credentials) {
+    public String validateUser(Credentials credentials) {
         if (credentials != null) {
             Query query = new Query();
             query.addCriteria(Criteria.where("credentials.username").is(credentials.getUsername()));
             User user = mongoTemplate.findOne(query, User.class);
             if (user != null && user.getCredentials().getPassword().equals(credentials.getPassword()))
-                return user.getId();
+                return user.getId().toString();
         }
         return null;
     }
 
-    public User getUser(ObjectId currentId) {
-        if (currentId != null) {
+    public User getUser(String currentIdString) {
+        if (currentIdString != null) {
+            ObjectId currentId = new ObjectId(currentIdString);
             User user = mongoTemplate.findById(currentId, User.class);
+
+            // make sure user exists
+            if (user == null)
+                return null;
 
             // get bank accounts
             Query query = new Query();
@@ -47,11 +52,27 @@ public class UserDao {
         return null;
     }
 
-    public void setUser(User changedUser, ObjectId currentId) {
-        if (changedUser != null && currentId != null) {
-            Query query = new Query();
-            query.addCriteria(Criteria.where("_id").is(currentId));
-            mongoTemplate.findAndReplace(query, changedUser);
+    public void setUser(User changedUser, String currentIdString) {
+        if (currentIdString != null && changedUser != null) {
+            ObjectId currentId = new ObjectId(currentIdString);
+            if (!currentId.equals(changedUser.getId()))
+                return;
+
+            // get user from DB
+            User oldUser = mongoTemplate.findById(currentId, User.class);
+
+            // balance should not be changed here and credentials/forgotPassword also should not be null
+            if (oldUser == null || oldUser.getBalance() != changedUser.getBalance()
+                    || changedUser.getCredentials() == null
+                    || changedUser.getForgotPassword() == null)
+                return;
+
+            // not stored in UserCollection
+            changedUser.setBankAccountList(null);
+            changedUser.setStockList(null);
+
+            // update user
+            mongoTemplate.save(changedUser);
         }
     }
 }
