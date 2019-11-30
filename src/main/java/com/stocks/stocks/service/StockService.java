8a990670b04135260abbc1a7acae5a830b16f7f5
AshWije <ashiniwije126@gmail.com>
2019-11-30 12:43:19 -0600

    Update StockService.java

--- src/main/java/com/stocks/stocks/service/StockService.java
+++ src/main/java/com/stocks/stocks/service/StockService.java
@@ -1,9 +1,9 @@
 package com.stocks.stocks.service;
 
 import com.stocks.stocks.dao.StockDao;
+import com.stocks.stocks.model.Schedule;
 import com.stocks.stocks.model.Stock;
 import com.stocks.stocks.model.StockPriceHistory;
-import com.stocks.stocks.model.StockWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -22,42 +22,49 @@ public class StockService {
         this.stockDao = stockDao;
     }
 
-    public Stock getCurrentStockPriceFromTickerSymbol(String tickerSymbol, HttpServletRequest request) {
+    public List<Stock> getStocks(String property, String value, HttpServletRequest request) {
         HttpSession session = request.getSession();
         if (session != null && session.getAttribute("USERNAME") != null)
-            return stockDao.getCurrentStockPriceFromTickerSymbol(tickerSymbol);
+            return stockDao.getStocks(property, value);
         return null;
     }
 
-    public List<Stock> getCurrentStockPriceFromCompany(String company, HttpServletRequest request) {
+    public double getCurrentStockPrice(String stockId, HttpServletRequest request) {
         HttpSession session = request.getSession();
         if (session != null && session.getAttribute("USERNAME") != null)
-            return stockDao.getCurrentStockPriceFromCompany(company);
+            return stockDao.getCurrentStockPrice(stockId);
+        return -1;
+    }
+
+    public StockPriceHistory getStockPriceHistory(String stockId, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        if (session != null && session.getAttribute("USERNAME") != null)
+            return stockDao.getStockPriceHistory(stockId);
         return null;
     }
 
-    public StockPriceHistory getStockPriceHistoryFromTickerSymbol(String tickerSymbol, HttpServletRequest request) {
-        HttpSession session = request.getSession();
-        if (session != null && session.getAttribute("USERNAME") != null)
-            return stockDao.getStockPriceHistoryFromTickerSymbol(tickerSymbol);
-        return null;
-    }
-
-    public void buyStocks(List<Stock> stockList, HttpServletRequest request) {
+    public void buyStocks(List<String> stockIdList, HttpServletRequest request) {
         HttpSession session = request.getSession();
         if (session != null) {
-            Object username = session.getAttribute("USERNAME");
-            if (username != null)
-                stockDao.buyStocks(username.toString(), stockList);
+            String userId = (String)session.getAttribute("USER_ID");
+            if (userId != null)
+                stockDao.buyStocks(stockIdList, userId);
         }
     }
 
-    public void sellStocks(List<Stock> stockList, HttpServletRequest request) {
+    public void sellStocks(List<String> stockIdList, HttpServletRequest request) {
         HttpSession session = request.getSession();
         if (session != null) {
-            Object username = session.getAttribute("USERNAME");
-            if (username != null)
-                stockDao.sellStocks(username.toString(), stockList);
+            String userId = (String)session.getAttribute("USER_ID");
+            if (userId != null)
+                stockDao.sellStocks(stockIdList, userId);
         }
+    }
+
+    public List<Stock> getAllStocks(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        if (session != null && session.getAttribute("USERNAME") != null)
+            return stockDao.getAllStocks();
+        return null;
     }
 }
