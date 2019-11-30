2547b25056861922ad222e824d2c257449363a64
AshWije <ashiniwije126@gmail.com>
2019-11-30 12:59:39 -0600

    Update UserDao.java

--- src/main/java/com/stocks/stocks/dao/UserDao.java
+++ src/main/java/com/stocks/stocks/dao/UserDao.java
@@ -1,15 +1,14 @@
 package com.stocks.stocks.dao;
 
-
-import com.stocks.stocks.model.BankAccount;
-import com.stocks.stocks.model.Credentials;
-import com.stocks.stocks.model.Stock;
-import com.stocks.stocks.model.User;
+import com.stocks.stocks.model.*;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Repository("userDao")
 public class UserDao {
@@ -30,10 +29,10 @@ public class UserDao {
         return null;
     }
 
-    public User getUser(String currentIdString) {
-        if (currentIdString != null) {
-            ObjectId currentId = new ObjectId(currentIdString);
-            User user = mongoTemplate.findById(currentId, User.class);
+    public User getUser(String userIdString) {
+        if (userIdString != null) {
+            ObjectId userId = new ObjectId(userIdString);
+            User user = mongoTemplate.findById(userId, User.class);
 
             // make sure user exists
             if (user == null)
@@ -41,25 +40,35 @@ public class UserDao {
 
             // get bank accounts
             Query query = new Query();
-            query.addCriteria(Criteria.where("userId").is(currentId));
+            query.addCriteria(Criteria.where("userId").is(userId));
             user.setBankAccountList(mongoTemplate.find(query, BankAccount.class));
 
             // get stocks
-            user.setStockList(mongoTemplate.find(query, Stock.class));
+            List<Stock> stockList = new ArrayList<Stock>();
+            for (ObjectId stockId : user.getStockIdList()) {
+                Stock stock = mongoTemplate.findById(stockId, Stock.class);
+                if (stock == null)
+                    return null;
+                stockList.add(stock);
+            }
+            user.setStockList(stockList);
+
+            //get schedules
+            user.setScheduleList(mongoTemplate.find(query, Schedule.class));
 
             return user;
         }
         return null;
     }
 
-    public void setUser(User changedUser, String currentIdString) {
-        if (currentIdString != null && changedUser != null) {
-            ObjectId currentId = new ObjectId(currentIdString);
-            if (!currentId.equals(changedUser.getId()))
+    public void setUser(User changedUser, String userIdString) {
+        if (userIdString != null && changedUser != null) {
+            ObjectId userId = new ObjectId(userIdString);
+            if (!userId.equals(changedUser.getId()))
                 return;
 
             // get user from DB
-            User oldUser = mongoTemplate.findById(currentId, User.class);
+            User oldUser = mongoTemplate.findById(userId, User.class);
 
             // balance should not be changed here and credentials/forgotPassword also should not be null
             if (oldUser == null || oldUser.getBalance() != changedUser.getBalance()
@@ -70,6 +79,8 @@ public class UserDao {
             // not stored in UserCollection
             changedUser.setBankAccountList(null);
             changedUser.setStockList(null);
+            changedUser.setStockIdList(null);
+            changedUser.setScheduleList(null);
 
             // update user
             mongoTemplate.save(changedUser);
