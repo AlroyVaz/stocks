6d51bb15ceebda9a4f46daee0abb7845f998dfe3
AshWije <ashiniwije126@gmail.com>
2019-12-03 23:13:03 -0600

    Update UserDao.java

--- src/main/java/com/stocks/stocks/dao/UserDao.java
+++ src/main/java/com/stocks/stocks/dao/UserDao.java
@@ -18,6 +18,35 @@ public class UserDao {
         this.mongoTemplate = mongoTemplate;
     }
 
+    public boolean addUser(User user) {
+        if (user != null) {
+            if (mongoTemplate.findById(user.getId(), User.class) != null)
+                return false; // user id already exists
+
+            if (user.getCredentials() == null || user.getCredentials().getUsername() == null || user.getCredentials().getPassword() == null || user.getForgotPassword() == null)
+                return false; // user credentials/forgotPassword don't exist or are invalid
+
+            Query query = new Query();
+            query.addCriteria(Criteria.where("credentials.username").is(user.getCredentials().getUsername()));
+            if (mongoTemplate.find(query, User.class).size() > 0)
+                return false; // another user has the same username
+
+            if (user.getBalance() != 0)
+                return false; // user balance isn't initially zero
+
+            // not stored in UserCollection (should be null on registration)
+            user.setBankAccountList(null);
+            user.setStockList(null);
+            user.setStockIdList(null);
+            user.setScheduleList(null);
+
+            // add user to db
+            mongoTemplate.save(user);
+            return true;
+        }
+        return false;
+    }
+
     public String validateUser(Credentials credentials) {
         if (credentials != null) {
             Query query = new Query();
@@ -64,7 +93,7 @@ public class UserDao {
         return null;
     }
 
-    public void setUser(User changedUser, String userIdString) {
+    public void updateUser(User changedUser, String userIdString) {
         if (userIdString != null && changedUser != null) {
             ObjectId userId = new ObjectId(userIdString);
             if (!userId.equals(changedUser.getId()))
@@ -76,17 +105,44 @@ public class UserDao {
             // balance should not be changed here and credentials/forgotPassword also should not be null
             if (oldUser == null || oldUser.getBalance() != changedUser.getBalance()
                     || changedUser.getCredentials() == null
+                    || changedUser.getCredentials().getUsername() == null
+                    || changedUser.getCredentials().getPassword() == null
                     || changedUser.getForgotPassword() == null)
                 return;
+
+            // make sure username is unique
+            Query query = new Query();
+            query.addCriteria(Criteria.where("credentials.username").is(changedUser.getCredentials().getUsername()));
+            if (mongoTemplate.find(query, User.class).size() > 0)
+                return; // another user has the same username
+
+            // stock id list should not be changed
+            changedUser.setStockIdList(oldUser.getStockIdList());
 
             // not stored in UserCollection
             changedUser.setBankAccountList(null);
             changedUser.setStockList(null);
-            changedUser.setStockIdList(null);
             changedUser.setScheduleList(null);
 
             // update user
             mongoTemplate.save(changedUser);
         }
+    }
+
+    public boolean forgotPassword(String username, String forgotPassword) {
+        if (username != null && forgotPassword != null) {
+            // find user in db
+            Query query = new Query();
+            query.addCriteria(Criteria.where("credentials.username").is(username));
+            User user = mongoTemplate.findOne(query, User.class);
+
+            // ensure user exists
+            if (user == null)
+                return false;
+
+            // check if forgot password secret is correct
+            return user.getForgotPassword().equals(forgotPassword);
+        }
+        return false;
     }
 }
