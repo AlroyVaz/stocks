a2038021e708ebfe2769fbf71a98ed4e5e6d66f5
AshWije <ashiniwije126@gmail.com>
2019-12-03 23:11:24 -0600

    Update LoginController.java

--- src/main/java/com/stocks/stocks/api/LoginController.java
+++ src/main/java/com/stocks/stocks/api/LoginController.java
@@ -3,10 +3,7 @@ package com.stocks.stocks.api;
 import com.stocks.stocks.model.Credentials;
 import com.stocks.stocks.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -24,5 +21,10 @@ public class LoginController {
     @PostMapping
     public void login(@RequestBody Credentials credentials, HttpServletRequest request) {
         loginService.login(credentials, request);
+    }
+
+    @PostMapping("/{username}/forgotPassword")
+    public void forgotPassword(@PathVariable("username") String username,@RequestBody String forgotPassword, HttpServletRequest request) {
+        loginService.forgotPassword(username, forgotPassword, request);
     }
 }
