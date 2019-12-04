8ccf705024722d70c5777afec18842ef624ce924
AshWije <ashiniwije126@gmail.com>
2019-12-03 23:11:51 -0600

    Create RegistrationController.java

---
+++ src/main/java/com/stocks/stocks/api/RegistrationController.java
@@ -0,0 +1,27 @@
+package com.stocks.stocks.api;
+
+import com.stocks.stocks.model.User;
+import com.stocks.stocks.service.RegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequestMapping("register")
+@RestController
+public class RegistrationController {
+    private final RegistrationService registrationService;
+
+    @Autowired
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    @PostMapping
+    public void addUser(@RequestBody User user, HttpServletRequest request) {
+        registrationService.addUser(user, request);
+    }
+}
